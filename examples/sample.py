e6a583f9f29ba2f0d0b53a098c241f5f7b730724
macbook <isaevnextdoor@gmail.com>
2017-11-20 18:56:52 +0300

    issue#6 fix

--- examples/sample.py
+++ examples/sample.py
@@ -1,17 +1,9 @@
 import time
 
 import disneylandClient.disneyland_pb2
-from disneylandClient.disneyland_pb2 import ListJobsRequest, ListOfJobs, Job, RequestWithId
+from disneylandClient.disneyland_pb2 import ListJobsRequest, Job, RequestWithId
 
-
-def gen(i=0):
-    while i < 2:
-        yield ListJobsRequest(how_many=1, kind="test")
-        i += 1
-        time.sleep(0.1)
-
-
-print("\tUser:")
+print("\tUser able to:")
 
 user_client = disneylandClient.new_client()
 
@@ -53,31 +45,3 @@ print("success\n{0}".format(created_job))
 print("\tDelete Job")
 deleted_job = user_client.DeleteJob(RequestWithId(id=blank_job.id))
 print("success\n{0}".format(deleted_job))
-
-print ("\tCreate multiple Jobs")
-created_jobs = user_client.CreateMultipeJobs(ListOfJobs(jobs=[Job(kind="docker"), Job(kind="test")]))
-print (created_jobs)
-
-print ("\tBidi RPC")
-stream = user_client.BidiJobs(gen())
-for job in stream:
-    print (job)
-
-print("\tWorker:")
-
-worker_client = disneylandClient.new_client_from_path("/Users/macbook/.disney/config2.yml")
-print("\tGet Job")
-read_job = worker_client.GetJob(RequestWithId(id=created_job.id))
-print("success\n{0}".format(read_job))
-
-print("\tModify Job")
-read_job.input = "worker-input"
-read_job.metadata = "worker_test"
-
-updated_job = worker_client.ModifyJob(read_job)
-print("success\n{0}".format(updated_job))
-
-print("\tPull Jobs with proper kind")
-pulled_jobs = worker_client.PullPendingJobs(ListJobsRequest(how_many=1))
-if len(pulled_jobs.jobs) > 0:
-    print("success\n{0}".format(pulled_jobs))
